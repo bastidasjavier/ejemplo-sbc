b4191741d4b32163c444c198a9104c0f6bbee1f9
Carlos David Bullones Jimenez <carlos.bullones@ngncloud.net>
2016-05-26 14:11:01 -0430

    Modificación del menú principal del sitio.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,4 +1,43 @@
-/************************* Owl carousel ***************************************/
+/*** Function JS/Jquery ***/
+
+/**** Carousel Bootstrap Home ****/
+$('.carousel').carousel({
+   interval: 5000
+  });
+
+/**** SmothScroll****/
+smoothScroll.init();
+
+
+/**** Menu ****/
+(function() {
+
+  "use strict";
+
+  var toggles = document.querySelectorAll(".c-hamburger");
+  var nav = document.getElementById("nav-menu");
+
+  for (var i = toggles.length - 1; i >= 0; i--) {
+    var toggle = toggles[i];
+    toggleHandler(toggle);
+  };
+
+  function toggleHandler(toggle) {
+    toggle.addEventListener( "click", function(e) {
+      e.preventDefault();
+      (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
+      (nav.classList.contains("menu-expanded") === true) ? nav.classList.remove("menu-expanded") : nav.classList.add("menu-expanded");
+    });
+  }
+
+  $(".nav-btn").click(function(){
+    $("nav[id=nav-menu]").removeClass("menu-expanded");
+    $("button[id=btn-hamburger]").removeClass("is-active");
+  });
+
+})();
+
+/**** Owl carousel ****/
  $(document).ready(function() {
               var owl = $('.owl-carousel');
               owl.owlCarousel({
@@ -17,7 +56,7 @@
               })
             })
 
-/***************************** Up to top js ***************************************/
+/**** Up to top js ****/
 
 $(document).ready(function() {
         
@@ -25,10 +64,4 @@ $(document).ready(function() {
         
       });
 
-/******************************Carousel Bootstrap**********************************/
-$('.carousel').carousel({
-   interval: 5000
-  });
 
-/******************************SmothScroll*****************************************/
-smoothScroll.init();
