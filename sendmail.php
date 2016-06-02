71ba17c68a41b81895c67d7e675dfd4c130b1956
Carlos David Bullones Jimenez <carlos.bullones@ngncloud.net>
2016-06-02 11:00:29 -0430

    prueba error inducido: correo malo.

--- sendmail.php
+++ sendmail.php
@@ -8,10 +8,10 @@ function sendMail($email, $subject, $body)
     $mail->isSMTP();                                      // Set mailer to use SMTP
     $mail->Host = 'smtp.zoho.com';                        // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'soporte@ngncloud.com';                 // SMTP username
+    $mail->Username = 'sopor@ngncloud.com';                 // SMTP username
     $mail->Password = 'dae1e9d168ca6609df625234baf848b4';     // SMTP password
     $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-    $mail->Port = 46;                                    // TCP port to connect to
+    $mail->Port = 465;                                    // TCP port to connect to
 
     $mail->setFrom('soporte@ngncloud.com', 'DNComputing');
     $mail->addAddress($email, 'Contacto DNComputing');     // Add a recipient
